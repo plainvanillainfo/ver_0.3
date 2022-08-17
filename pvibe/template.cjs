1b52bd3232999ac5057d5f6063ee5d179150451c
Your Name <you@example.com>
2022-08-17 15:12:21 -0700

    u

--- pvibe/template.cjs
+++ pvibe/template.cjs
@@ -15,6 +15,21 @@ class TemplateItem {
     fromClient(message) {
         console.log("TemplateItem::fromClient(): ", message);
         if (message.Action != null) {
+            switch (message.Action) {
+                case 'ContinueTemplateElem':
+                    if (message.TemplateElem != null && message.TemplateElem.UseCaseElemName != null) {
+                        if(this.elems[message.TemplateElem.UseCaseElemName] != null) {
+                            this.elems[message.TemplateElem.UseCaseElemName].fromClient(message.TemplateElem);
+                        } else {
+                            let templateElemNew = new TemplateElem(this, this.useCase.elems[message.TemplateElem.UseCaseElemName]);
+                            this.elems[message.TemplateElem.UseCaseElemName] = templateElemNew;
+                            //templateElemNew.trigger();
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
@@ -40,8 +55,30 @@ class TemplateList {
 }
 
 class TemplateElem {
-    constructor(parent) {
-        this.UseCaseElem = null;
+    constructor(parent, useCaseElem) {
+        this.parent = parent;
+        this.useCaseElem = useCaseElem;
+        this.track = this.parent.track;
+        this.session = this.parent.session;
+        //this.model = this.parent.model;
+        this.itemParent = parent.item;
+        this.toClient = this.toClient.bind(this);
+        //this.trigger = this.trigger.bind(this);
+    }
+
+    fromClient(message) {
+        console.log("TemplateItem::fromClient(): ", message);
+    }
+
+    toClient(messageIn) {
+        let messageOut = {
+            Action: 'ContinueTemplateElem',
+            TemplateElem: {
+                UseCaseName: this.useCase.Detail.Name,
+                ...messageIn
+            }
+        };
+        this.parent.toClient(messageOut);
     }
 
 }
