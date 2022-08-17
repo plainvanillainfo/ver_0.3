e42794ce62c251b6875dbdc397112438dba831d2
Plain Vanilla <65374564+plainvanillainfo@users.noreply.github.com>
2022-08-16 19:04:26 -0700

    u

--- pvibe/template.cjs
+++ pvibe/template.cjs
@@ -2,7 +2,32 @@ const { UseCaseItem, UseCaseList, UseCaseElem } = require('./usecase.cjs');
 
 class TemplateItem {
     constructor(parent) {
-        this.UseCaseItem = null;
+        this.parent = parent;
+        this.track = this.parent.track;
+        this.elems = {};
+        this.key = null;
+        this.toClient = this.toClient.bind(this);
+    }
+
+    destroy() {
+    }
+
+    fromClient(message) {
+        console.log("TemplateItem::fromServer(): ", message);
+        if (message.Action != null) {
+        }
+    }
+
+    toClient(messageIn) {
+        let messageOut = {
+            Action: 'ContinueTemplateItem',
+            TemplateItem: {
+                UseCaseName: this.useCase.Detail.Name,
+                ItemId: this.key,
+                ...messageIn
+            }
+        };
+        this.parent.toClient(messageOut);
     }
 
 }
