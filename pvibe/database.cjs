278e2d741257e4ccef27056639d9cbcb40cdf69e
Your Name <you@example.com>
2022-08-18 14:08:29 -0700

    u

--- pvibe/database.cjs
+++ pvibe/database.cjs
@@ -28,13 +28,19 @@ class Database {
         this.client.query(
             'SELECT "Id" as "Id", "Detail" as "Detail" FROM public."FEUseCase" WHERE 1=1 ', 
             (err, res) => {
-            this.client.query(
-                'SELECT u."Id" as "UserId", u."Name" as "UserName", e."Id" as "EntitlementId", e."UseCase" as "UseCase", e."BaseObject" as "BaseObject" ' +
-                'FROM public."FEUser" u, public."FEEntitlement" e ' +
-                'WHERE u."Id" = \'' + messageIn.UserId + '\' AND u."Active" = TRUE AND e."UserId" = u."Id"',
-                (err1, res1) => {
-                    sendEntitlement(messageIn, res.rows, res1.rows);
-            });
+                if (err) {
+                } else {
+                    this.client.query(
+                        'SELECT u."Id" as "UserId", u."Name" as "UserName", e."Id" as "EntitlementId", e."UseCase" as "UseCase", e."BaseObject" as "BaseObject" ' +
+                        'FROM public."FEUser" u, public."FEEntitlement" e ' +
+                        'WHERE u."Id" = \'' + messageIn.UserId + '\' AND u."Active" = TRUE AND e."UserId" = u."Id"',
+                        (err1, res1) => {
+                            if (err1) {
+                            } else {
+                                sendEntitlement(messageIn, res.rows, res1.rows);
+                            }
+                        });
+                }
         });
     }
 
@@ -43,7 +49,10 @@ class Database {
         this.client.query(
             'SELECT * FROM public."' + view + '" WHERE '+filter, 
             (err, res) => {
-            sendViewResultToClient(res.rows);
+                if (err) {
+                } else {
+                    sendViewResultToClient(res.rows);
+                }
         });
     }
 
