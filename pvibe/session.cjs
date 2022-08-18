b9bddf9ee92046d2a5152b40f9359d338c2c202f
Your Name <you@example.com>
2022-08-17 21:55:13 -0700

    u

--- pvibe/session.cjs
+++ pvibe/session.cjs
@@ -106,7 +106,7 @@ class TrackServer {
         if (message.Action != null && message.TemplateItem != null) {
             if (this.templateItemRoot == null) {
                 let useCaseFound = this.session.entitlement.UseCases.find(
-                    useCaseCur => useCaseCur.Name === this.session.entitlement.Identity[0].UseCase);
+                    useCaseCur => useCaseCur.Id === this.session.entitlement.Identity[0].UseCase);
                 if (useCaseFound != null ) {
                     this.templateItemRoot = new TemplateItem(this, useCaseFound);
                 }
