a14e19f4440f126698913fe342784bdc53781983
Your Name <you@example.com>
2022-09-19 13:02:07 -0700

    u

--- pvibe/engineroom.cjs
+++ pvibe/engineroom.cjs
@@ -8,6 +8,51 @@ class EngineRoom {
 
 }
 
+class ChildProcess {
+    constructor(parent) {
+    }
+}
+
+class RESTApiSession {
+    constructor(parent) {
+    }
+}
+
+class FileTransfer {
+    constructor(parent) {
+    }
+}
+
+class FileFetch {
+    constructor(parent) {
+    }
+}
+
+class FileImport {
+    constructor(parent) {
+    }
+}
+
+class FileImportCSV {
+    constructor(parent) {
+    }
+}
+
+class FileImportPDF {
+    constructor(parent) {
+    }
+}
+
+class FileImportHTML {
+    constructor(parent) {
+    }
+}
+
+class FileImportXML {
+    constructor(parent) {
+    }
+}
+
 module.exports = {
     EngineRoom: EngineRoom
 }
