04b6b0848ed1a10fe6bdf19ea303221637ba9e34
Your Name <you@example.com>
2022-10-08 15:02:36 -0700

    u

--- pvife/template.cjs
+++ pvife/template.cjs
@@ -794,8 +794,7 @@ class TemplateList {
             this.useCase.Detail.Elems.forEach(elemCur => {
                 let tableItemRowCell = document.createElement('td');
                 tableItemRow.appendChild(tableItemRowCell);
-                let valueCur = itemCur.Attrs[elemCur.Name] != null ? itemCur.Attrs[elemCur.Name] : ''
-                valueCur = valueCur.substr(0, 1000);
+                let valueCur = itemCur.Attrs[elemCur.Name] != null ? itemCur.Attrs[elemCur.Name].substr(0,1000) : ''
                 tableItemRowCell.appendChild(document.createTextNode(valueCur));
             });
         });
