ec33e3056295be6b5c9d0f14e0e73819296d28eb
Your Name <you@example.com>
2022-11-28 15:20:16 -0800

    u

--- pvife/template.cjs
+++ pvife/template.cjs
@@ -6,19 +6,6 @@ class TemplateItem extends TemplateItemClient{
         this.divTarget = divTarget;
     }
 
-    
-    show() {
-        this.divTarget.appendChild(document.createTextNode(JSON.stringify(this.useCase)));
-    }
-    
-    hide() {
-        let child = this.divTarget.lastChild;
-        while (child) {
-            this.divTarget.removeChild(child);
-            child = this.divTarget.lastChild;
-        }
-    }
-    
     setUseCase(useCase) {
         this.useCase = useCase;
         console.log("TemplateItem::setUseCase(): ", this.useCase);
