6f5951a5ffc389354005c59c6066df55d29a8ca5
Your Name <you@example.com>
2022-11-28 14:38:57 -0800

    u

---
+++ pvibe/template_client.cjs
@@ -0,0 +1,58 @@
+class TemplateItemClient {
+    constructor(parent) {
+        this.parent = parent;
+        this.track = this.parent.track;
+        this.itemKey = null;
+        this.elems = {};
+        this.toServer = this.toServer.bind(this);
+    }
+
+    fromServer(message) {
+        console.log("TemplateItem::fromServer(): ", message);
+        if (message.Action != null) {
+            switch (message.Action) {
+                case 'ContinueTemplateElem':
+                    if (message.TemplateElem != null && message.TemplateElem.UseCaseElemName != null) {
+                        if (this.elems[message.TemplateElem.UseCaseElemName] != null) {
+                            this.elems[message.TemplateElem.UseCaseElemName].fromServer(message.TemplateElem);
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        if (message.Item != null) {
+            this.item = message.Item;
+            switch (this.useCase.Detail.Format) {
+                case 'Menu':
+                    this.setUseCaseMenu();
+                    break;
+                case 'Form':
+                    this.setUseCaseForm();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    toServer(messageIn) {
+        let messageOut = {
+            Action: 'ContinueTemplateItem',
+            TemplateItem: {
+                UseCaseName: this.useCase.Detail.Name,
+                ItemKey: this.itemKey,
+                ...messageIn
+            }
+        };
+        this.parent.toServer(messageOut);
+    }
+    
+
+}
+
+
+module.exports = {
+    TemplateItemClient: TemplateItemClient
+}
