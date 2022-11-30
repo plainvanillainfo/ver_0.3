a525eab106003b03ca7cda513ae80bbe18bb9d63
Your Name <you@example.com>
2022-11-30 09:37:03 -0800

    u

--- pvife/template.cjs
+++ pvife/template.cjs
@@ -8,11 +8,121 @@ class TemplateItem extends TemplateItemClient{
 
     renderUseCase() {
         console.log("TemplateItem::renderUseCase()");
-
     }
     
     renderDataItems() {
         console.log("TemplateItem::renderDataItems()");
+        switch (this.useCase.Detail.Cardinality) {
+            case 'Single':
+                if (this.dataItems.length === 1) {
+                    this.renderSingleDataItem(this.dataItems[0]);
+                }
+                break;
+            case 'Multiple':
+                this.renderMultipleDataItems(this.dataItems);
+                break;
+            default:
+                break;
+        }
+    }
+
+    renderSingleDataItem(dataItem) {
+        switch (this.useCase.Detail.Flow) {
+            case 'Parallel':
+                switch (this.useCase.Detail.Rendering.Format) {
+                    case 'Menu':
+                        this.presentMenu();
+                        break;
+                    case 'Radio':
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case 'Serial':
+                switch (this.useCase.Detail.Rendering.Format) {
+                    case 'Form':
+                        break;
+                    case 'Markup':
+                        break;
+                    default:
+                        break;
+                }
+                break;
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    renderMultipleDataItems(dataItems) {
+
+    }
+
+    presentMenu() {
+        console.log("TemplateItem::setUseCaseMenu");
+        this.nav = document.createElement('nav');
+        this.divTarget.appendChild(this.nav);
+        this.nav.className = 'navbar navbar-expand-md navbar-dark bg-primary';
+        this.divNav = document.createElement('div');
+        this.nav.appendChild(this.divNav);
+        this.divNav.className = 'container-fluid';
+        this.divTargetSub = document.createElement('div')
+        this.divTarget.appendChild(this.divTargetSub);
+        this.divTargetSub.style.margin = '10px';
+
+        this.buttonCollapse = document.createElement('button');
+        this.divNav.appendChild(this.buttonCollapse);
+        this.buttonCollapse.className = 'navbar-toggler';
+        this.buttonCollapse.setAttribute("type", "button");
+        this.buttonCollapse.setAttribute("data-bs-toggle", "collapse");
+        this.buttonCollapse.setAttribute("data-bs-target", "#menuContent");
+        this.buttonCollapse.setAttribute("aria-controls", "menuContent");
+        this.buttonCollapse.setAttribute("aria-expanded", "false");
+        this.buttonCollapse.setAttribute("aria-label", "Toggle navigation");
+
+        this.iconCollapse = document.createElement('span');
+        this.buttonCollapse.appendChild(this.iconCollapse);
+        this.buttonCollapse.className = 'navbar-toggler-icon';
+
+        this.divMenu = document.createElement('div');
+        this.divNav.appendChild(this.divMenu);
+        this.divMenu.id = 'menuContent';
+        this.divMenu.className = 'collapse navbar-collapse';
+
+        this.ulMenu = document.createElement('ul');
+        this.divMenu.appendChild(this.ulMenu);
+        this.ulMenu.className = 'navbar-nav me-auto mb-2 mb-md-0';
+        this.ulMenu.ItemLIs = [];
+        this.useCase.Detail.Elems.forEach(menuItemCur => {
+            let itemLICur;
+            itemLICur = document.createElement('li');
+            this.ulMenu.appendChild(itemLICur);
+            itemLICur.Label = menuItemCur.Label;
+            itemLICur.className = 'nav-item';
+            this.ulMenu.ItemLIs.push(itemLICur);
+            itemLICur.A = document.createElement('a');
+            itemLICur.appendChild(itemLICur.A);
+            itemLICur.A.className = 'nav-link';
+            itemLICur.A.setAttribute("href", "#");
+            itemLICur.A.appendChild(document.createTextNode(itemLICur.Label));
+            itemLICur.A.addEventListener('click', (event) => {
+                event.preventDefault();
+                console.log("TemplateItem::setUseCaseMenu - click on menu item", menuItemCur);
+                let elemPicked = this.useCase.Detail.Elems.find(elemCur => elemCur.Name === menuItemCur.Name);
+                if (this.elems[menuItemCur.Name] == null) {
+                    this.elems[menuItemCur.Name] = new TemplateElem(this, elemPicked, this.divTargetSub, false);
+                }
+                for (let elemCur in this.elems) {
+                    let elemDetail = this.elems[elemCur];
+                    if (elemDetail.useCaseElem != null && elemDetail.useCaseElem.Name !== menuItemCur.Name) {
+                        elemDetail.hide();
+                    }
+                }
+                this.elems[menuItemCur.Name].show();
+            });
+        });
     }
 
 }
