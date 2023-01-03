3d23a5946c7730d44cc452bdc527aac3c5e5122a
Your Name <you@example.com>
2023-01-02 17:54:52 -0800

    u

--- pvife/template.cjs
+++ pvife/template.cjs
@@ -65,7 +65,7 @@ class TemplateItem extends TemplateItemClient {
             inputSearch.setAttribute("placeholder", "Search..");
 
             let buttonClear = document.createElement('button');
-            inputSearch.appendChild(buttonClear);
+            divSearch.appendChild(buttonClear);
             buttonClear.setAttribute("type", "button");
             buttonClear.id = 'clearbutton';
             let iconClear = document.createElement('i');
