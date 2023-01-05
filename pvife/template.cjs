dc840d35ad16521de6b419d05cb6f1264e2ddf38
Your Name <you@example.com>
2023-01-04 16:33:54 -0800

    u

--- pvife/template.cjs
+++ pvife/template.cjs
@@ -310,6 +310,7 @@ class TemplateItem extends TemplateItemClient {
         */
 
     presentTable() {
+        alert(this.useCase.Detail);
         this.tableList = document.createElement('table');
         this.divItem.appendChild(this.tableList);
         this.tableList.className = 'table table-hover table-striped caption-top table-responsive';
