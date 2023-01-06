867bf00d80dc59f022ab949579a7211c31de0dc0
Your Name <you@example.com>
2023-01-06 10:55:53 -0800

    u

--- pvife/template.cjs
+++ pvife/template.cjs
@@ -404,7 +404,7 @@ class TemplateItem extends TemplateItemClient {
                 this.itemCells[itemCur.Key].push({...cellCur});
             });
 
-            console.log('AAA isLeaf: ', this.isLeaf, '- ', itemCur);
+            //console.log('AAA isLeaf: ', this.isLeaf, '- ', itemCur);
 
             tableOwner.columns.forEach(colCur => {
                 let cellCur = this.itemCells[itemCur.Key].find(cur => cur.Col === colCur);
