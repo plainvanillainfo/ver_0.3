6a259bdd70017ba0ea7ab422c9e9fd0654fbf7e2
plainvanillainfo <plainvanillainfo@gmail.com>
2023-01-17 17:17:21 -0800

    u

--- pvibe/template.cjs
+++ pvibe/template.cjs
@@ -313,7 +313,7 @@ class TemplateElem {
 										console.log("UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU");
 										this.templateItem = new TemplateItem(this, subUseCaseFound);
 										if (message.TemplateItem.TemplateItem != null) {
-											console.log("aaaaaaaaaaaaaaaaaaaaaa:", this.itemParent); //, "\n", this.parent, "\n", this.parent.parent ); //.itemList);
+											console.log("aaaaaaaaaaaaaaaaaaaaaa:", this.itemParent, "\n", this.parent); //, "\n", this.parent.parent ); //.itemList);
 											this.templateItem.fromClient(message.TemplateItem.TemplateItem);
 										} else {
 											console.log("bbbbbbbbbbbbbbbbbbb");
