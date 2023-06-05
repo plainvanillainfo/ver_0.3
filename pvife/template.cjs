f4d3c459fbbdf5627a8ab6151638587fc8041ee3
plainvanillainfo <plainvanillainfo@gmail.com>
2023-06-05 10:23:16 -0700

    u

--- pvife/template.cjs
+++ pvife/template.cjs
@@ -14,6 +14,7 @@ class TemplateItem extends TemplateItemClient {
         this.isLeaf = true;
         this.columns = [];
         this.templateItemCoercer = this;
+        this.dataItems = [];
         this.itemCells = {};
         this.itemCellsParent = [];
         /* 
