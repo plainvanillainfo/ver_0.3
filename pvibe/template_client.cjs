dcda9538fedf87fc296543ddceac3a846dbdf77e
plainvanillainfo <plainvanillainfo@gmail.com>
2023-06-04 20:25:20 -0700

    u

--- pvibe/template_client.cjs
+++ pvibe/template_client.cjs
@@ -39,6 +39,7 @@ class TemplateItemClient {
         };
         if (messageIn.TemplateItem != null && messageIn.TemplateItem.TemplateElem != null && 
                 messageIn.TemplateItem.TemplateElem.Action !== 'Start' && messageIn.TemplateItem.TemplateElem.ItemKey != null) {
+            messageOut.ItemKey = messageIn.TemplateItem != null && messageIn.TemplateItem.TemplateElem != null && messageIn.TemplateItem.TemplateElem.ItemKey;
             messageOut.TemplateItem.ItemKey = messageIn.TemplateItem != null && messageIn.TemplateItem.TemplateElem != null && messageIn.TemplateItem.TemplateElem.ItemKey;
         }
         this.parent.toServer(messageOut);
