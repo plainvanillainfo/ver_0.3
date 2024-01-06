91edd8b8882d4bd5ed495cd0a7c729951e817bfa
plainvanillainfo <plainvanillainfo@gmail.com>
2024-01-05 17:09:12 -0800

    u

--- pvibe/database.cjs
+++ pvibe/database.cjs
@@ -3,6 +3,8 @@ const { Pool, Client } = require('pg')
 class Database {
     constructor(parent, dbConfig) {
         this.parent = parent;
+        this.dbConfig = dbConfig;
+        /*
         this.client = new Client({
             user: dbConfig.User,
             host: dbConfig.Host,
@@ -10,24 +12,102 @@ class Database {
             password: dbConfig.Password,
             port: dbConfig.Port
         })
+        */
+        this.client = new Client({
+            user: this.dbConfig.User,
+            host: this.dbConfig.Host != null ? this.dbConfig.Host : this.dbConfig.LocalHost,
+            database: this.dbConfig.Database,
+            password: this.dbConfig.Password,
+            port: this.dbConfig.Port != null ? this.dbConfig.Port : this.dbConfig.LocalPort
+        })
     }
+
+    /*
+        async start() {
+        console.log("Database::start()");
+        if (this.dbConfig.Tunnel != null) {
+            const { Client } = await import('ssh2');
+            console.log("Database::start() - await import('ssh2') returned");
+            var sshConnection = new Client();
+            sshConnection.on('ready', () => {
+                sshConnection.forwardOut(this.dbConfig.Tunnel.host,
+                    this.dbConfig.Tunnel.port,
+                    this.dbConfig.RemoteHost,
+                    this.dbConfig.RemotePort,
+                    (err, stream) => {
+                        if (err) {
+                            sshConnection.end();
+                            return deny();
+                        }
+                        this.startDbClient();
+                    });
+            }).on('error', (err) => {
+                deny();
+            }).connect(this.dbConfig.Tunnel);
+        } else {
+            this.startDbClient();
+        }
+    }
+    */
     
     async openDataDB(setConfigFromDB) {
         console.log("Database::openDataDB()");
+        if (this.dbConfig.Tunnel != null) {
+            const { Client } = await import('ssh2');
+            console.log("Database::openDataDB() - await import('ssh2') returned");
+            var sshConnection = new Client();
+            sshConnection.on('ready', () => {
+                sshConnection.forwardOut(this.dbConfig.Tunnel.host,
+                    this.dbConfig.Tunnel.port,
+                    this.dbConfig.RemoteHost,
+                    this.dbConfig.RemotePort,
+                    (err, stream) => {
+                        if (err) {
+                            sshConnection.end();
+                            return deny();
+                        }
+                        this.startDbClient();
+                    });
+            }).on('error', (err) => {
+                deny();
+            }).connect(this.dbConfig.Tunnel);
+        } else {
+            //this.startDbClient();
+            await this.client.connect();
+            console.log("Database::openDataDB() - await returned");
+            this.client.on('error', (err) => {
+                console.error("Database::openDataDB() - client.on('error')", err.stack)
+            })
+            this.client.query(
+                'SELECT * FROM functionality."AppConfig" ORDER BY "Param" ASC',
+                (err, res) => {
+                setConfigFromDB(res.rows);
+            })
+        }
+    }
+       
+    async startDbClient() {
+        console.log("Database::startDbClient()");
         await this.client.connect();
-        console.log("Database::openDataDB() - await returned");
+        console.log("Database::startDbClient() - await this.client.connect() returned");
         this.client.on('error', (err) => {
-            console.error("Database::openDataDB() - client.on('error')", err.stack)
-        })
-        this.client.query(
-            'SELECT * FROM functionality."AppConfig" ORDER BY "Param" ASC',
-            (err, res) => {
-            setConfigFromDB(res.rows);
-        })
+            console.error("Database::startDbClient() - client.on('error')", err.stack)
+            this.client = null;
+        });
     }
 
     async openDataDBStartClient(clientConnectedToDB) {
         console.log("Database::openDataDBStartClient()");
+        // This is called by sys/server_monitor.cjs
+        /*
+        this.client = new Client({
+            user: this.dbConfig.User,
+            host: this.dbConfig.Host,
+            database: this.dbConfig.Database,
+            password: this.dbConfig.Password,
+            port: this.dbConfig.Port
+        })
+        */
         await this.client.connect();
         console.log("Database::openDataDBStartClient() - await returned");
         this.client.on('error', (err) => {
